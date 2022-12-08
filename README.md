cd115328993eed84542b24a6e15fb61cec88767b
enescaglarr <112442158+enescaglarr@users.noreply.github.com>
2022-12-08 20:57:31 +0300

    Update and rename README.py to README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# my-project
+<h1> Github Workshop </h1>
+  
+<p> <a href="https://www.google.com"> Held by </a></p>
+
--- README.py
+++
@@ -1,9 +0,0 @@
-# my-project
-<h1> Github Workshop </h1>
-  
-<p> <a href="https://www.google.com"> Held by </a></p>
-
-my_list=["Great!","Good","Very Good!"]
-in=input("How was workshop?") 
-if in my_list:
-  print("Well done!")
